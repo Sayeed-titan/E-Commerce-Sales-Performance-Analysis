9cb22d48de0b52a2aa68cfb59659289603de0cba
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-11-22 18:39:57 +0600

    Update ECommerceSalesDB_DDL.sql
    Create tables customers, products, orders, order details

--- ECommerceSalesDB_DDL.sql
+++ ECommerceSalesDB_DDL.sql
@@ -14,3 +14,39 @@ GO
 
 USE ECommerceSalesDB;
 GO
+
+-- Create Tables ---
+
+CREATE TABLE Customers(
+	CustomerID INT PRIMARY KEY,
+	CustomerName VARCHAR(100),
+	Email VARCHAR(100),
+	City VARCHAR(50),
+	Region VARCHAR(50),
+	Cost DECIMAL(10,2)
+);
+
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    UnitPrice DECIMAL(10,2),
+    Cost DECIMAL(10,2)
+);
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID),
+    OrderDate DATE,
+    ShippingDate DATE,
+    Status VARCHAR(20)
+);
+
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+    Quantity INT,
+    Discount DECIMAL(5,2)
+);
+
