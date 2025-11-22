1f3d051afcdfd8040ad5eedbd29bedc2ac9b3375
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-11-22 18:31:11 +0600

    Create ECommerceSalesDB_DDL.sql
    Add DDL - create database

---
+++ ECommerceSalesDB_DDL.sql
@@ -0,0 +1,16 @@
+
+-- =====================================================
+-- PROJECT: E-Commerce Sales Performance Analysis
+-- Description: Analyze sales data to identify trends,
+--              top products, and regional performance
+-- Skills Demonstrated: Complex SQL, Joins, Subqueries,
+--                      CTEs, Window Functions, Aggregations
+-- =====================================================
+
+-- Create Database Schema ---
+
+CREATE DATABASE ECommerceSalesDB;
+GO
+
+USE ECommerceSalesDB;
+GO
